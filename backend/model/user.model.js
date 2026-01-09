6c43eb07d941dd885517117da1c927051ddfbe56
rahidkhan <rahidkhanbannu1@email.com>
2026-01-09 08:16:37 +0430

    create userModel

---
+++ backend/model/user.model.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+import { mongoose } from 'mongoose';
+const userSchema = new mongoose.Schema({
+     fullname:{
+          type:String,
+          required:true,
+
+     },
+     email:{
+          type:String,
+          required:true,
+          unique:true,
+          
+     },
+     password:{
+        type:String,
+        required:true,
+        
+     },
+     phone:{
+          type:String,
+          required:true,
+     
+     },
+     role:{
+          type:String,
+          required:true,
+          enum:['user','deliveryboy', 'customer'],
+     }
+},{timestamps:true});
+
+
+const UserModel =mongoose.model("User", userSchema);
+export default UserModel;
+
+ 
